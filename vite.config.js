729ac149d1e3dcee4c75f4b7249218307539114a
vinayakverma <vinayakverma5050@gmail.com>
2026-07-01 13:08:59 +0530

    first commit

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      // Force Vite to use the version of React in your root node_modules
+      'react': path.resolve(__dirname, 'node_modules/react'),
+      'react-dom': path.resolve(__dirname, 'node_modules/react-dom'),
+    },
+  },
+});
+
+
