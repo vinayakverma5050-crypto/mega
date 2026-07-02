cb4bfcabc48ef8db49416da67049a1e1d7f4b1f0
vinayakverma <vinayakverma5050@gmail.com>
2026-07-02 10:41:10 +0530

    gem-vcon

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,10 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
-  
-  resolve: {
-       alias: {
-
-      // Force Vite to use the version of React in your root node_modules
-      'react': path.resolve(__dirname, 'node_modules/react'),
-      'react-dom': path.resolve(__dirname, 'node_modules/react-dom'),
-    }, 
-  }, 
 });
-
-
